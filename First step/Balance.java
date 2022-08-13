6a50e32243dd4e02abba55ccb4f1713873e0215c
sw88tch <fcukmycopyrights@gmail.com>
2022-08-13 18:10:13 -0400

    change and add new file Balance

---
+++ First step/Balance.java
@@ -0,0 +1,7 @@
+public class Balance {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 0; i -= 1) {
+            System.out.println("Balance card: " + i);
+        }
+    }
+}
