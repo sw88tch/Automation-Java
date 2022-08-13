4a4d2b6385505b51742107484a0cc81ff9fa5fc7
sw88tch <fcukmycopyrights@gmail.com>
2022-08-13 17:48:26 -0400

    Initial commit

---
+++ First step/Praktikum.java
@@ -0,0 +1,7 @@
+public class Praktikum {
+    public static void main(String[] args) {
+        for (int i = 27; i <= 30; i += 1) {
+            System.out.println("Nabrano ballov: " + i);
+        }
+    }
+}
