09f2cd9fd42e209ba71c89aba1f3e3ed473e6539
sw88tch <fcukmycopyrights@gmail.com>
2022-08-13 17:59:17 -0400

    add new class

---
+++ First step/Apricots.java
@@ -0,0 +1,8 @@
+public class Apricots {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 12; i += 1) {
+            System.out.println(i + "-y apricot ate");
+        }
+        System.out.println("Apricots done! Let's go study.");
+    }
+}
