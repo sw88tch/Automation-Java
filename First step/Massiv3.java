f0823a95504bebb1902f6904f141207025190973
sw88tch <fcukmycopyrights@gmail.com>
2022-08-18 08:51:11 -0400

    add massiv3

---
+++ First step/Massiv3.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+public class Massiv3 {
+    public static void main(String[] args){
+        String[] appliances = { "Washer", "Dryer", "Frozen", "Microwave", "TV", "Mixer" };
+        String[] additionalAppliances = { "VacuumCleaner", "BathSupplies", "NewItem" };
+        String[] newAppliances = new String[9];
+        for (int i = 0; i < appliances.length; i++) {
+            newAppliances[i] = appliances[i];
+        }
+        for (int i = 0; i < additionalAppliances.length; i++) {
+            newAppliances[i + 6] = additionalAppliances[i];
+        }
+        for (int i = 0; i < newAppliances.length; i++) {
+            System.out.println("Naimenovanie: " + newAppliances[i]);
+        }
+    }
+}
