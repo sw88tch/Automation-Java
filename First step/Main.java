dfb1abe5cf68aa80f0b35792f561949a96ebf3f3
sw88tch <fcukmycopyrights@gmail.com>
2023-02-20 02:19:33 +0300

    add rename

---
+++ First step/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        MelonStorage melonStorage = new MelonStorage(300);
+        System.out.println(melonStorage.getMelonAmount());
+        melonStorage.reduceMelonAmount(100);
+        System.out.println(melonStorage.getMelonAmount());
+    }
+}
+
+class MelonStorage {
+    private int melonAmount;
+    public MelonStorage(int newMelonAmount) {
+        melonAmount = newMelonAmount;
+    }
+    public void reduceMelonAmount(int amountToTake) {
+        if (amountToTake > melonAmount) {
+            int difference = amountToTake - melonAmount;
+            System.out.println("Ne udalos' vyvezti: " + difference);
+            melonAmount = 0;
+        } else {
+            melonAmount = melonAmount - amountToTake;
+        }
+    }
+
+    public int getMelonAmount() {
+        return melonAmount;
+    }
+}
